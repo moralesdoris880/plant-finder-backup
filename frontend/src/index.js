3b48f2a91790d15cfd3f783a2d2c37e27e025fd0
petermanos <manospete@gmail.com>
2024-03-10 17:03:31 -0400

    Created pages and added about content

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LearnMore from './pages/LearnMore';
+import ZipCode from './pages/ZipCode';
+import Contact from './pages/Contact';
+import MeetDevelopers from './pages/MeetDevelopers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> 
+    <Routes> 
+      <Route exact strict path='/'element={<App />} />
+      <Route exact path='/About'element={<About />} />
+      <Route exact path='/LearnMore'element={<LearnMore />} />
+      <Route exact path='/ZipCode'element={<ZipCode />} />
+      <Route exact path='/About/Contact'element={<Contact />} />
+      <Route exact path='/About/MeetDevelopers'element={<MeetDevelopers />} />
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals();  
