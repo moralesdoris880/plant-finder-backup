df4e51bba8a63fb185f0e0e24efa33df2959d766
naialehtinen <136741900+naialehtinen@users.noreply.github.com>
2024-03-19 19:28:06 -0400

    MeetDevelopers js + css, Contact js + css

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,4 +1,18 @@
+import * as React from 'react';
+import './Contact.css';
 function Contact() {
-    return (<div></div>)
+    return (
+      <div >
+        <div>
+            <h1> Contact us! </h1>
+            <br></br>
+            <h2> Feel free to reach out with any inquiries! </h2>
+            <br></br>
+            <p1> (xxx) xxx-xxxx  </p1>
+            <br></br>
+            <p1> xxxx@gmail.com  </p1>
+        </div>
+      </div>
+      )
   }
   export default Contact;
