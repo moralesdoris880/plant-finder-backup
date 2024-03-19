04f0ee2b2db271409cfc53182ee76bf83e60d726
DanielaUrbano <107948064+DanielaUrbano@users.noreply.github.com>
2024-03-19 18:30:37 -0400

    added text context learn more and css

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,7 +4,7 @@ function NavBar() {
     return(
         <div id="NavBar">
             <h4 id="Logo">PLANTFINDER</h4>
-            <span class="material-symbols-outlined" id="burger" >menu</span>
+            <span class="material-symbols-outlined" id="burger" > menu</span>
         </div>
     )
 }
