17c1b30fd46fa7d039fa3637ed327eee1eec1430
Doris Morales <moralesdoris880@gmail.com>
2024-03-26 16:17:45 -0400

    comment testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import "mapbox-gl/dist/mapbox-gl.css";
 function App() {
   return (
     <div className="App">
+      {/* this is a comment */}
       <div id="Map1">
         <SearchBar />
         <Map
