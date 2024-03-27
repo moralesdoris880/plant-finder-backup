ef1e90412217181b646e15a6653ecb03275aa3e9
naialehtinen <136741900+naialehtinen@users.noreply.github.com>
2024-03-27 07:53:21 -0400

    Added personal bio

--- frontend/src/pages/MeetDevelopers.js
+++ frontend/src/pages/MeetDevelopers.js
@@ -25,19 +25,18 @@ function MeetDevelopers() {
             <div class="grid-item">
               <img src="/Naia.jpg" alt="Profile"  className='profile'/>          
               <h2> Naia Lehtinen</h2>
-              <p1> Student at CUNY Hunter College and currently employed Part-time at Compass. My major is in Environmental Studies and I hope to pursue other opportunities through internships, volunteering and working.   </p1>
+              <p1> Undergraduate student at CUNY Hunter College majoring in Environmental Studies, minoring in Computer Science, and gaining a Certificate in Business Studies. Currently interning as a Public Service Scholar at the Teaching Kitchen and have previous experience in government work, nonprofit work and real estate. My interests include photography, fashion, and fostering environmental awareness.  </p1>
             </div>
             <div class="grid-item">
               <img src="/profile.png" alt="Profile"  className='profile'/>          
               <h2> Daniela Urbano</h2>
               <p1> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p1>
-              <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
             </div>
+            <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
           </div>
-         
       </div>
     </div>
     )
