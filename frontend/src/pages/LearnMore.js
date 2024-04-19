7be52df6988547ad3e05fc75aaa440e46b5ea956
Doris Morales <moralesdoris880@gmail.com>
2024-04-19 16:37:31 -0400

    fixed minor css on learn more page

--- frontend/src/pages/LearnMore.js
+++ frontend/src/pages/LearnMore.js
@@ -1,29 +1,34 @@
 import * as React from 'react';
 import './LearnMore.css';
+import Footer from '../components/Footer';
+
 function LearnMore() {
-    return (<div>
-      <h1>WHY ARE NATIVE PLANTS IMPORTANT?</h1>
-<br></br>
+    return (
+    <div id="LearnMoreContainer">
+      <h1 className='LearnMoreTitle'>WHY ARE NATIVE PLANTS IMPORTANT?</h1>
+      <br></br>
       <p4> 
-As climate change becomes more and more of a threat, native plants become more and more important. 
-Our native plants provide essential ecosystem services and have specifically evolved to deal with 
-an areas’ microclimatic conditions. Species evolve alongside each other in communities, and their 
-successful function relies on all the other species. By planting native, you not only gain aesthetic improvement, 
-but help out all the other organisms that your area calls home. Learn more about Native Plants from the National Audobon Society 
-<a href="https://www.audubon.org/content/why-native-plants-matter">Here! </a>
-</p4>
-<h1>WHATS THE DEAL WITH INVASIVE AND NOXIOUS SPECIES?</h1>
-<br></br>
-<p4>While noxious species may be native, they can be harmful to the wellbeing of humans and other animals,
-  and for that reason, any plantings should be done after consulting with local arborists or ecologists to 
-  ensure your safety. Invasive species might not be directly harmful to you, but can do damage to the ecosystem 
-  and lead to environmental degradation over time. While some invasive species may be beautiful, their impacts 
-  on our communities are not. Learn more about invasive and noxious species from the Bureau of Land Management 
-  <a href="https://www.blm.gov/programs/weeds-and-invasives/about">Here! </a> </p4>
-  <br></br>
-  <br></br>
-  <br></br>
-  <br></br>
+        As climate change becomes more and more of a threat, native plants become more and more important. 
+        Our native plants provide essential ecosystem services and have specifically evolved to deal with 
+        an areas’ microclimatic conditions. Species evolve alongside each other in communities, and their 
+        successful function relies on all the other species. By planting native, you not only gain aesthetic improvement, 
+        but help out all the other organisms that your area calls home. Learn more about Native Plants from the National Audobon Society 
+        <a href="https://www.audubon.org/content/why-native-plants-matter">Here! </a>
+      </p4>
+      <h1 className='LearnMoreTitle'>WHATS THE DEAL WITH INVASIVE AND NOXIOUS SPECIES?</h1>
+      <br></br>
+      <p4>While noxious species may be native, they can be harmful to the wellbeing of humans and other animals,
+        and for that reason, any plantings should be done after consulting with local arborists or ecologists to 
+        ensure your safety. Invasive species might not be directly harmful to you, but can do damage to the ecosystem 
+        and lead to environmental degradation over time. While some invasive species may be beautiful, their impacts 
+        on our communities are not. Learn more about invasive and noxious species from the Bureau of Land Management 
+        <a href="https://www.blm.gov/programs/weeds-and-invasives/about">Here! </a> 
+      </p4>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      < Footer />
     </div>)
   }
   export default LearnMore;
