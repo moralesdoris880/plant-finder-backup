67225fdcd240a116c09034b6c378e63a3b2d2837
DanielaUrbano <107948064+DanielaUrbano@users.noreply.github.com>
2024-04-20 23:18:15 -0400

    created the new cards

---
+++ frontend/src/components/Cardcontainer.js
@@ -0,0 +1,37 @@
+import * as React from "react";
+const CardContainer = () => {
+  return (
+    <div className="pageBody">
+      <div className="container">
+        <div className="card">
+          <div className="imgbox">
+            <img className="Card-image" src="lavendar.webp" alt="plant"></img>
+          </div>
+          <div className="content">
+            <h1> lavendar </h1>
+            <p> scientific_name</p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="imgbox">
+            <img className="Card-image" src="lavendar.webp" alt="plant"></img>
+          </div>
+          <div className="content">
+            <h1> lavendar </h1>
+            <p> scientific_name</p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="imgbox">
+            <img className="Card-image" src="lavendar.webp" alt="plant"></img>
+          </div>
+          <div className="content">
+            <h1> lavendar </h1>
+            <p> scientific_name</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default CardContainer;
